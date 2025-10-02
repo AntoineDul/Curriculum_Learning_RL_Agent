d3373468b024146dba129e259093fdcc830ab1d2
AntoineDul <antoine.dulauroy@gmail.com>
2025-10-01 20:41:51 -0400

    Moving goal implemented and repo structure improved.

--- source/main.py
+++ source/main.py
@@ -1,20 +1,19 @@
-from environment import GridWorld
-from test_agent import RandomAgent
+from modules.environment.grid_world import GridWorld
+from modules.agent.random_agent import RandomAgent
 
 def main():
-    env = GridWorld(n=10, max_steps=100)
+    env = GridWorld(n=10, max_steps=100, lbda=0.99, obstacle_density=0.2, debug=True)
     agent = RandomAgent()
    
     obs = env._get_obs()
-    done = False
     iterations = 0
 
-    while not done:
+    while not env.done:
         action = agent.get_action()
-        obs, reward, done = env.step(action)
+        obs, reward = env.step(action)
         env.render()
         iterations += 1
-        print(f"Obs : {obs}, Reward : {reward}, Action: {action}, Iter: {iterations}")
+        print(f"Obs : {obs}, Reward : {reward}, Action: {action}, Steps: {env.steps}, Total Iter: {iterations}")
 
 
 if __name__ == "__main__":
---
+++ source/modules/environment/grid_world.py
@@ -0,0 +1,173 @@
+import math
+import random
+import numpy as np
+# import gym
+
+from modules.environment.goal import Goal
+
+class GridWorld:
+    def __init__(self, n=5, max_steps=100, lbda=0.9, obstacle_density=0.2, debug=False):
+        self.n = n
+        self.max_steps = max_steps
+        self.lbda = lbda
+        self.obstacle_density = obstacle_density  
+        self.goal = Goal(n= self.n, is_moving=True, move_frequency=5, debug=debug)
+        self.done = False
+        self.reset()
+        self.debug = debug
+ 
+    def reset(self):
+        """
+        Reset environment:
+            - Gives a random position to the agent and goal
+            - Add obstacles in random places of the grid        
+        """
+
+        # Define agent position
+        self.agent_pos = tuple(int(x) for x in np.random.randint(0, self.n, size=2, dtype=int))
+        self.goal.reset()
+
+        # Add random obstacles 
+        self.obstacles = set()
+        for _ in range(math.floor(self.obstacle_density * (self.n**2) )):
+            pos = tuple(int(x) for x in np.random.randint(0, self.n, size=2, dtype=int))
+            if pos != self.agent_pos and pos != self.goal.position:
+                self.obstacles.add(pos) 
+
+        # Initialize the number of steps 
+        self.steps = 0
+
+        print(f"agent : {self.agent_pos} \ngoal : {self.goal.position}\n obstacles: {self.obstacles}")
+
+    def _get_obs(self):  #TODO: change for diff of vectors (goal - agent pos)
+        """
+        Gives the current state of the env
+
+        Returns: np.array(...)
+        """
+        gx, gy = self.goal.position
+        ax, ay = self.agent_pos
+        diff_vector = (gx - ax, gy - ay)
+
+        return [diff_vector, self.obstacles]
+
+    def step(self, agent_action):
+        """
+        Executes the action chosen by the agent and updates the environment
+        So far, agent moves before goal
+
+        Inputs:
+            agent_action (int) : action to execute (0 = up, 1 = right, 2 = down, 3 = left)
+
+        Returns:
+            tuple[obs, reward]:
+                first element is the observation of the environment after the action,
+                the second is the reward earned by the agent from the action
+        """
+
+        # Check if action is successful
+        if random.random() > self.lbda:
+            agent_action = np.random.choice([0, 1, 2, 3])
+
+            # Debug
+            if self.debug:
+                print("Environment chooses a random action")
+
+        # Check action legal and handles it
+        legal, new_agent_position = self.is_legal(self.agent_pos, agent_action)
+        if legal:
+            self.agent_pos = new_agent_position
+
+            # Update steps
+            self.steps += 1
+            reward = 0
+
+            # Check goal
+            if self.agent_pos == self.goal.position:
+                reward = 1
+                self.done = True
+
+            # Check max steps
+            if self.steps >= self.max_steps:
+                self.done = True
+
+            # Move goal if needed
+            if self.goal.is_moving and self.steps % self.goal.move_frequency == 0:
+                
+                # Asks goal where it wants to move 
+                goal_action = np.random.choice([0, 1, 2, 3])
+                
+                # Compute action and checks if it is legal
+                legal, new_goal_position = self.is_legal(self.goal.position, goal_action, self.goal.step_size)
+
+                # Update goal position if legal else do not move goal (could change -> loop or change goal.step_size)
+                if legal:
+                    self.goal.set_position(new_goal_position)
+
+        # Action illegal
+        else:
+            if self.debug:
+                print("skip turn")
+            reward = 0
+
+        return self._get_obs(), reward
+
+    def is_legal(self, initial_position, action, step_size=1):
+        """
+        Determine if an action is legal and returns resulting position 
+
+        Args: 
+            initial_position (np.array) : starting position 
+            action (int) : action to execute
+            step_size (int) : size of the displacement
+            
+        Returns:
+            tuple[bool, np.array]:
+                first element indicates if action is legal, second returns new position
+        """
+        new_position = None
+        if action == 0:  # Up
+            new_position = tuple([initial_position[0], initial_position[1] - step_size])
+        elif action == 1:  # Right
+            new_position = tuple([initial_position[0] + step_size, initial_position[1]])
+        elif action == 2:  # Down
+            new_position = tuple([initial_position[0], initial_position[1] + step_size])
+        elif action == 3:  # Left
+            new_position = tuple([initial_position[0] - step_size, initial_position[1]])
+        else:
+            return (False, None)  # Invalid action
+
+        # Boundary check
+        if not (0 <= new_position[0] < self.n and 0 <= new_position[1] < self.n):
+            # Debug
+            if self.debug: 
+                print(new_position)
+                print("Illegal move, boundary!")
+            return (False, None)
+
+        # Obstacle check
+        if new_position in self.obstacles:
+            if self.debug:
+                print(new_position)
+                print("Illegal move, obstacle!")
+            return (False, None)
+
+        return (True, new_position)
+
+    def render(self):
+        """
+        Render the environment as a string
+        """
+        grid = np.full((self.n, self.n), ".")
+        ax, ay = self.agent_pos
+        gx, gy = self.goal.position
+        grid[ax, ay] = "A"  # Agent
+        grid[gx, gy] = "G"  # Goal
+
+        for obstacle in self.obstacles:  # Obstacles
+            ox, oy = obstacle
+            grid[ox, oy] = "X"
+
+        # Print grid
+        for row in grid:
+            print(" ".join(row))
